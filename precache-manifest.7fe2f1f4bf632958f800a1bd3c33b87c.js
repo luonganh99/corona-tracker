555474e7b3ed2ef0d861a17a4c8b8bc42ec15b58
luonganh99 <nguyenhuuluonganh@gmail.com>
2020-08-05 23:23:58 +0700

    Updates

---
+++ precache-manifest.7fe2f1f4bf632958f800a1bd3c33b87c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f13ea8a5c3e50b9bb5f0a77756f901",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "c6a56b96eb75880fc8fd",
+    "url": "/corona-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "9e1d010daea471b2a26c",
+    "url": "/corona-tracker/static/css/main.28f16d71.chunk.css"
+  },
+  {
+    "revision": "c6a56b96eb75880fc8fd",
+    "url": "/corona-tracker/static/js/2.448ad6f8.chunk.js"
+  },
+  {
+    "revision": "1672046551382c0b01e77eeceb0ab6c4",
+    "url": "/corona-tracker/static/js/2.448ad6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1d010daea471b2a26c",
+    "url": "/corona-tracker/static/js/main.36645d46.chunk.js"
+  },
+  {
+    "revision": "6e1275481ab408ab4c66",
+    "url": "/corona-tracker/static/js/runtime-main.f8245450.js"
+  },
+  {
+    "revision": "8f95c3462abc698639bbb3fa453ed460",
+    "url": "/corona-tracker/static/media/blue-1.8f95c346.png"
+  },
+  {
+    "revision": "7bca717fe28ec4e73a2ec84448f6f994",
+    "url": "/corona-tracker/static/media/blue-2.7bca717f.png"
+  },
+  {
+    "revision": "3fd044dc540c9eb96c997efa266137c7",
+    "url": "/corona-tracker/static/media/blue-3.3fd044dc.png"
+  },
+  {
+    "revision": "4cb0ad816d638e67bed4eabbcacf43b9",
+    "url": "/corona-tracker/static/media/death.4cb0ad81.svg"
+  },
+  {
+    "revision": "83ddbd421ba5878471cc80c119252242",
+    "url": "/corona-tracker/static/media/hcmus_poster.83ddbd42.jpg"
+  },
+  {
+    "revision": "7fc09419229a3adcee2e478921669171",
+    "url": "/corona-tracker/static/media/how_to_reg.7fc09419.svg"
+  },
+  {
+    "revision": "3cf91e946db72d5df9eda1bc30f34a26",
+    "url": "/corona-tracker/static/media/logo.3cf91e94.png"
+  },
+  {
+    "revision": "91b69d5e8689d0372051e01c2531194b",
+    "url": "/corona-tracker/static/media/verified_user.91b69d5e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c858a4451887f705f3e816e0a269840.js"
+  "/corona-tracker/precache-manifest.7fe2f1f4bf632958f800a1bd3c33b87c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corona-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
